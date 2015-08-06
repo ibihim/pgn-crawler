ebeddf607519d553bb02d01f925fa2b952b5c70b
Krzysztof Ostrowski <kao@posteo.de>
2015-08-07 00:22:43 +0200

    transition to events

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 "use strict";
 
-const pgnLinkHarvester = require("./lib/harvest-pgn-links");
+const playerId = "82276";
+const PgnLinkHarvester = require("./lib/harvest-pgn-links");
+const pgnLinkHarvester = PgnLinkHarvester.create(playerId);
+const pgnLinkEmitter = pgnLinkHarvester.pgnLinkEmitter;
+
+var foundLinks = [];
+
+pgnLinkEmitter.on("linksFound", function (links) {
+    console.log(`adding ${links.length} links`);
+    foundLinks.push(links);
+});
+
+pgnLinkEmitter.on("linksComplete", function () {
+    console.log(`found ${foundLinks.length} links`);
+    console.log(foundLinks);
+})
+
+pgnLinkHarvester.start()
