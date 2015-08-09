4ae8575c0c6ee75d541dba4b8ac1a80e8435d345
Krzysztof Ostrowski <kao@posteo.de>
2015-08-09 15:19:32 +0200

    trying it with callbacks

--- index.js
+++ index.js
@@ -1,24 +1,4 @@
 "use strict";
 
-const HarvestPgnGames = require("./lib/harvest-pgn-games");
-const PgnLinkHarvester = require("./lib/harvest-pgn-links");
+const pgn = require("./lib/pgn");
 
-const playerId = "82276";
-const harvestPgnGames = HarvestPgnGames.create(playerId);
-const pgnLinkHarvester = PgnLinkHarvester.create(playerId);
-
-const pgnLinkEmitter = pgnLinkHarvester.pgnLinkEmitter;
-
-pgnLinkEmitter.on("linksFound", function (links) {
-    console.log(`adding ${links.length} links`);
-
-    harvestPgnGames.getPgn(links[0], function (pgn) {
-        console.log(pgn);
-    })    
-});
-
-pgnLinkEmitter.on("linksComplete", function () {
-    console.log("no more links to go for");
-})
-
-pgnLinkHarvester.start()
