d71744f889249ab18be2d7452a4cb0de7ff591ab
Krzysztof Ostrowski <kao@posteo.de>
2015-08-09 23:20:11 +0200

    need to test

--- index.js
+++ index.js
@@ -2,3 +2,27 @@
 
 const pgn = require("./lib/pgn");
 
+const playerName = "Magnus Carlsen";
+const startTime = Date.now();
+
+function printPgns(pgns) {
+    const runTime = getRuntime();
+
+    console.log(pgns);
+
+    printRuntime(runTime);
+}
+
+function getRuntime() {
+    return (Date.now() - startTime)/1000;
+}
+
+function printRuntime(runTime) {
+    console.log(`links search had a runtime of ${runTime}s`);
+}
+
+pgn.getPgnsForPlayer(playerName, printPgns);
+
+//module.exports = function (playerName, callback) {
+//    pgn.getPgnsForPlayer(playerName, callback);    
+//}
