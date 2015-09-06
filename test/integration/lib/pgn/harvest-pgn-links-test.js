f8f9b897bc1a72fd0869d639e0d05beff0369564
Krzysztof Ostrowski <kao@posteo.de>
2015-09-06 21:27:26 +0200

    seems to work for links and id

--- test/integration/lib/pgn/harvest-pgn-links-test.js
+++ test/integration/lib/pgn/harvest-pgn-links-test.js
@@ -5,16 +5,13 @@ const PgnLinkStream = require("../../../../lib/pgn/harvest-pgn-links").PgnLinkSt
 const pgnLinkStream = new PgnLinkStream("62383");
 const chessGameLink = "http://www.chessgames.com/perl/nph-chesspgn?text=1&gid=";
 
-//const winston = require("winston");
-//winston.level = "debug";
-
 describe("harvest pgn links", function () {
     it("should return an expected number of pgns for a given player", function (done) {
         pgnLinkStream.on("readable", function () {            
             var read = pgnLinkStream.read();
             console.log("readable");
             if (read && read.toString) {
-                console.log(read.toString())
+                console.log(`chunk: ${read.toString()}`)
             } else {
                 console.log("null");
             }           
